607f4edb3202dd59fc093f99706054af924cd264
ChiaraGadda <153713137+ChiaraGadda@users.noreply.github.com>
2026-04-01 15:38:21 +0200

    Errore concettuale
    Correzzione di un errore concettuale riguardante xp e yp iniziale che si presenta in tutti quei casi dove l'intervallo della funzione non ha l'estremo inferiore in 0

--- integrali.cpp
+++ integrali.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main(){
-    double x,y,xp=0,yp=0,area=0;
-    while(cin>>x>>y){
-        //calcolo area trapezio e sommo
-        area+=(x-xp)*(y+yp)/2;
-        xp=x;
-        yp=y;
+    double x, y, xp, yp, area = 0;
+    cin >> xp >> yp;
+    while(cin >> x >> y){
+        area += (x - xp) * (y + yp) / 2;
+        xp = x;
+        yp = y;
     }
-    cout<<area;
-    return area;
+
+    cout << area;
+    return 0;
 }
