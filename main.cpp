21b56a5ffbfdce1248170bb4eeb4ce9b497e0224
ChiaraGadda <153713137+ChiaraGadda@users.noreply.github.com>
2026-04-01 15:53:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
  std::cin>>c1>>c2>>c3>>a>>b>>n;
  double s=(b-a)/n; //lo calcolo qui, così ogni volta non devo farlo nel for visto cche da lo stesso ris
 	
- for(int i=0; i<n; i++) {
+ for(int i=0; i<=n; i++) {
   double x = a + i * s;
   double y = c1 + c2 * x + c3 * x * x;
   std::cout << x << "\t" << y << std::endl; //se devo solo stampare posso fare i calcoli qua
