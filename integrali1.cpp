48c68be7076ddc8e79ab543a1db2485eb8eee31b
ChiaraGadda <gaddachiara06@gmail.com>
2026-04-07 20:12:22 +0200

    v2.1

--- integrali1.cpp
+++ integrali1.cpp
@@ -4,14 +4,11 @@
 using namespace std;
 
 int main(){
-    //double c1, c2, c3;
     double a, b;
     unsigned int n;
     string fun;
 
-    //leggi_funct(fun);
     cin >> fun;
-    //leggi_f(c1, c2, c3);
     leggi_dd(a, b);
     leggi_i(n);
 
@@ -24,10 +21,8 @@ int main(){
 
     double s = (b - a) / n;
 
-        cerr << "Funzione: " << fun <<" ["<<a<<","<<b<<"] n="<<n; 
     for(unsigned int i = 0; i <= n; i++) {
         double x = a + i * s;
-        //cout << x << "\t" << f(x, c1, c2, c3) << endl;
         cout << x << "\t" << calc_funct(fun, x) << endl;
     }
     return 0;
